03b1de3144d252597af3c2ee34c17d0303c4bd60
b4031639 <b4031639@TERRAN01>
2017-11-28 13:36:44 +0000

    added interration methods

--- ncltech/PhysicsNode.cpp
+++ ncltech/PhysicsNode.cpp
@@ -4,7 +4,23 @@
 
 void PhysicsNode::IntegrateForVelocity(float dt)
 {
-	/* TUTORIAL 2 CODE */
+	//gravity
+	if (invMass > 0.0f) {
+		linVelocity += PhysicsEngine::Instance()->GetGravity() * dt;
+	}
+
+	//semi implicit euler
+	//TODO replace with rk4 if time
+	linVelocity += force * invMass * dt;
+
+	//velocity damping
+	linVelocity = linVelocity * PhysicsEngine::Instance()->GetDampingFactor();
+
+	//angular rotation
+	angVelocity += invInertia * torque * dt;
+
+	//angular velocity damping
+	angVelocity = angVelocity *  PhysicsEngine::Instance()->GetDampingFactor();
 }
 
 /* Between these two functions the physics engine will solve for velocity
@@ -14,7 +30,13 @@ void PhysicsNode::IntegrateForVelocity(float dt)
 
 void PhysicsNode::IntegrateForPosition(float dt)
 {
-	/* TUTORIAL 2 CODE */
+	//update the position
+	position += linVelocity * dt;
+
+	//update orientation
+	orientation = orientation * Quaternion(angVelocity * dt * 0.5f, 0.0f);
+
+	orientation.Normalise();
 
 	//Finally: Notify any listener's that this PhysicsNode has a new world transform.
 	// - This is used by GameObject to set the worldTransform of any RenderNode's. 
